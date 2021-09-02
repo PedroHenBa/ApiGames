1fcc18dc1bba25fe16cc01ba862e4d8953f6f6de
Pedro Henrique <pedroh.ba04@gmail.com>
2021-09-01 23:24:10 -0300

    Configurando cors para consumo de api com axios

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const connection = require('./database/Connection');
 const routerGames = require('./Routes/games');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
